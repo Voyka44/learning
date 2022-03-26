bfa712496afde4777af36620b4d886abe80fd60f
Voyka44 <skobelkinv@mail.ru>
2022-03-27 00:36:02 +0300

    Обработка заголовков

--- parse.py
+++ parse.py
@@ -1,17 +1,8 @@
 import requests
-from requests.exceptions import HTTPError
-
-print("Пробуем скачать справочник БИК с сайта БР...")
-
-for url in ['https://api.github.com', 'https://api.github.com/invalid', 'https://ya.ru', 'https://cbr.ru/s/newbik']:
-    try:
-        response = requests.get(url)
-
-        # если ответ успешен, исключения задействованы не будут
-        response.raise_for_status()
-    except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')  # Python 3.6
-    except Exception as err:
-        print(f'Other error occurred: {err}')  # Python 3.6
-    else:
-        print('Подключаемся к ' + url + ' ...Success!')
+response = requests.post('https://httpbin.org/post', json={'key':'value'})
+json_response = response.json()
+json_response['data']
+'{"key": "value"}'
+json_response['headers']['Content-Type']
+'application/json'
+print(json_response)
