e363ae640692c1ab2c84e2131bbc8d1f12f34ae7
Voyka44 <skobelkinv@mail.ru>
2022-03-26 19:45:57 +0300

    Изучаею раоту с модулем, метод get

--- parse.py
+++ parse.py
@@ -1,3 +1,9 @@
 import requests
 
 print("Пробуем скачать справочник БИК с сайта БР...")
+r = requests.get('https://cbr.ru/s/newbik')
+print(r)
+if r.status_code == 200:
+    print("Success...")
+elif r.status_code == 403:
+    print("Not Found")
