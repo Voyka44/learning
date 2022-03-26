d3d7e9559b6f0b324ac29e89fe8c50df6af483d6
Voyka44 <skobelkinv@mail.ru>
2022-03-27 01:16:39 +0300

    Добавил таймаут

--- parse.py
+++ parse.py
@@ -2,7 +2,9 @@ import requests
 import zipfile
 
 print('Обновление справочника БИК с сайта БР...')
-Arch_name = open(r'C:\temp\bik.zip', "wb")
-url = requests.get('https://cbr.ru/s/newbik')
-Arch_name.write(url.content)
-Arch_name.close()
+path_save = 'c:\\temp\\'
+Arch_name = 'bik.zip'
+f = open(path_save + Arch_name, "wb")
+url = requests.get('https://cbr.ru/s/newbik', timeout=15.001)
+f.write(url.content)
+f.close()
