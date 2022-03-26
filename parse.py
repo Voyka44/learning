cc01a2a4bbab12e7a2dd8110933523c5d6e83854
Voyka44 <skobelkinv@mail.ru>
2022-03-26 21:32:54 +0300

    Изучаею работу с модулем Request. Обработка ошибок с сервера

--- parse.py
+++ parse.py
@@ -1,9 +1,17 @@
 import requests
+from requests.exceptions import HTTPError
 
 print("Пробуем скачать справочник БИК с сайта БР...")
-r = requests.get('https://cbr.ru/s/newbik')
-print(r)
-if r.status_code == 200:
-    print("Success...")
-elif r.status_code == 403:
-    print("Not Found")
+
+for url in ['https://api.github.com', 'https://api.github.com/invalid', 'https://ya.ru', 'https://cbr.ru/s/newbik']:
+    try:
+        response = requests.get(url)
+
+        # если ответ успешен, исключения задействованы не будут
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')  # Python 3.6
+    except Exception as err:
+        print(f'Other error occurred: {err}')  # Python 3.6
+    else:
+        print('Подключаемся к ' + url + ' ...Success!')
