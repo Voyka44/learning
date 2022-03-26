612d54149683bcd51869170d29949d3543a5989c
Voyka44 <skobelkinv@mail.ru>
2022-03-27 01:50:11 +0300

    Расскоментировал секцию загрузки

--- parse.py
+++ parse.py
@@ -2,16 +2,18 @@ import requests
 import zipfile
 import configparser
 
+#from setuptools.sandbox import save_path
+
 config = configparser.ConfigParser()  # создаём объекта парсера
 config.read("parse.ini")  # читаем конфиг
 
 
 print('Обновление справочника БИК с сайта БР...')
 
-# f = open(path_save + Arch_name, "wb")
-# url = requests.get('https://cbr.ru/s/newbik', timeout=15.001)
-# f.write(url.content)
-# f.close()
+f = open(config['Path']['save_path'] + config['Name']['arch_name'], "wb")
+url = requests.get('https://cbr.ru/s/newbik', timeout=15.001)
+f.write(url.content)
+f.close()
 z = zipfile.ZipFile(config['Path']['save_path'] + config['Name']['arch_name'])
 
 # читаем содержимое файла
