bcd1e0b1c2c01ecd1626f9b3d35f4cf0a9b376d2
Voyka44 <skobelkinv@mail.ru>
2022-03-27 12:11:57 +0300

    Получение имени файла архива из заголовка страницы

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
+from fileinput import filename
+
 import requests
 
 # Изучаем заголовки ответа
 r = requests.get('https://cbr.ru/s/newbik')
-print(r.headers)
+#print(r.headers)
+print(r.headers['Content-Disposition'])
+print(r.headers['Content-Disposition'][-20:])
